fa287e15849b05c96398a2288b0c24b1118af302
ArtisticCollective <83891742+ArtisticCollective@users.noreply.github.com>
2022-02-18 15:39:27 +0530

    Initial Commit

---
+++ SimpleOCR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleOCR
+//
+//  Created by Shahbaz Singh on 2022-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleOCR/SimpleOCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleOCRApp.swift
+//  SimpleOCR
+//
+//  Created by Shahbaz Singh on 2022-02-18.
+//
+
+import SwiftUI
+
+@main
+struct SimpleOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
